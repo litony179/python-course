15b870c2abdab32e843d0b309708e99a4a118f72
TonyYFL <litony179@gmail.com>
2021-04-14 23:11:02 +1000

    split method

--- module7/forLoop.py
+++ module7/forLoop.py
@@ -21,34 +21,3 @@ for i in myList:
 genre = ['pop', 'rock', 'classic']
 for i in range(len(genre)):
     print('I like to listen to ' + genre[i])
-
-# =======================================================================================================================
-# The Python split() method breaks up a string at the specified separator and
-# and returns a list of strings
-
-n = 'Tony.Angie.Will.Jaz.Kel.Emma'
-name = n.split('.')
-print(name)
-
-# We can also use lists as a form of database.
-# This following code snippet is an example
-
-database = [
-    ['Ali', 'Tony', 'Angie'],
-    ['Kim', 'Li', "Li"],
-    [23, 22, 25],
-]
-
-userName = database[0]
-familyName = database[1]
-age = database[2]
-
-input1 = str(input('please enter user or password'))
-
-# We can do queries on the database by writing a if construct
-if input1 == 'f' or input1 == 'firstname':
-    print(userName)
-elif input1 == 'l' or input1 == 'lastname':
-    print(familyName)
-else:
-    print(age)
